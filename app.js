5c06997528fa8fc5ec1d8fa50b6c9ebacf3afbb1
gitakpaudyal <gitakpaudyal@gmail.com>
2021-06-10 13:49:00 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const myArray = [6, 19, 20];
+const yourArray = [19, 81, 2];
+for (let i = 0; i < myArray.length; i++) {
+  for (let j = 0; j < yourArray.length; j++) {
+    if (myArray[i] === yourArray[j]) {
+      console.log('Both loops have the number: ' + yourArray[j])
+    }
+  }
+};
+
+
+
+/*let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
+//console.log(bobsFollowers);
+
+let tinasFollowers = ['Sam', 'Marta', 'Elle'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  console.log(j);
+   
+  }
+};
+*/
